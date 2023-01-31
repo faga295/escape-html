b33ff6d8ad41b1039adbc24e238c72fb41e6a7a3
faga <lzc295592@163.com>
2023-02-01 00:02:21 +0800

    chore(release): publish

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -3,5 +3,5 @@ import test from 'ava'
 import { escapeHtml } from '../index'
 
 test('sync function from native code', (t) => {
-    t.is(escapeHtml("<"), "&lt;")
+    t.is(escapeHtml("<div>"), "&lt;div&gt;")
 })
